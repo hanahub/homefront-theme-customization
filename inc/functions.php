c081c389c489ed2ec9527e34983180ec869c9330
Bishoy Awad <mazaa@outlook.com>
2018-05-16 20:38:47 +0200

    Removing shop top product search

--- inc/functions.php
+++ inc/functions.php
@@ -10,3 +10,19 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+// Removing header top bar
+add_action( 'wp', 'mhshop_removing_header_topbar', 99 );
+
+function mhshop_removing_header_topbar() {
+	if ( class_exists( 'Storefront_WooCommerce_Customiser' ) ) {
+		$search = get_theme_mod( 'swc_header_search', true );
+	} else {
+		$search = false;
+	}
+
+	if ( true != $search ) {
+		remove_action( 'storefront_header', 'storefront_product_search', 40 );
+		remove_action( 'storefront_header', 'storefront_product_search', 10 );
+	}
+}
